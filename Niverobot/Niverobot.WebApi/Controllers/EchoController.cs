a789127b0e512e23ae71a06a65a504b3cd60d757
Rasjaad <rasjaad10@msn.com>
2020-03-18 13:37:44 +0100

    Testing endpoints

---
+++ Niverobot/Niverobot.WebApi/Controllers/EchoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Niverobot.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class EchoController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public IActionResult Post([FromBody]string request)
+        {
+            return Ok(request);
+        }
+
+        // PUT api/<controller>/5
+
+    }
+}
