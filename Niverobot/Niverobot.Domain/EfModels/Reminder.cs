39562dcf88ce211205e487f23e74928eb5288b04
Rasjaad <rasjaad10@msn.com>
2020-03-22 14:45:15 +0100

    Added model

---
+++ Niverobot/Niverobot.Domain/EfModels/Reminder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Niverobot.Domain.EfModels
+{
+    class Reminder
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+    }
+}
