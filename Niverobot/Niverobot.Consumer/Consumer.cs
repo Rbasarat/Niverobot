ed4ec1ecc6b284348d2767e7204f67745f9fca14
Rasjaad <rasjaad10@msn.com>
2020-03-30 17:42:16 +0200

    Added Consumer

---
+++ Niverobot/Niverobot.Consumer/Consumer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Niverobot.Interfaces;
+using Serilog;
+
+namespace Niverobot.Consumer
+{
+    public class Consumer
+    {
+        private readonly IConfiguration _config;
+        private readonly IReminderService _reminderService;
+
+        public Consumer(IConfiguration config, IReminderService reminderService)
+        {
+            _config = config;
+            _reminderService = reminderService;
+        }
+
+        // Application starting point
+        public void Run()
+        {
+        }
+    }
+}
--- Niverobot/Niverobot.Consumer/Program.cs
+++ Niverobot/Niverobot.Consumer/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
-
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 namespace Niverobot.Consumer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create service collection and configure our services
+            var services = ConfigureServices();
+            // Generate a provider
+            var serviceProvider = services.BuildServiceProvider();
+            try
+            {
+                // Kick off our actual code
+                serviceProvider.GetService<Consumer>().Run();
+            }
+            finally
+            {
+                // Flush logs when application is finished.
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+            // Set up the objects we need to get to configuration settings
+            var config = LoadConfiguration();
+
+            // Set up logger.
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+
+
+            // Add the config to our DI container for later user
+            services.AddSingleton(config);
+
+            // IMPORTANT! Register our application entry point
+            services.AddTransient<Consumer>();
+
+            return services;
+        }
+
+        private static IConfiguration LoadConfiguration()
+        
+        {   var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            return builder.Build();
         }
     }
 }
