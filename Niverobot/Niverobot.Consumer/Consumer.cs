985428c2e0d9672dcaa79e30f89c582fbcc46478
Rasjaad <rasjaad10@msn.com>
2020-03-31 16:15:15 +0200

    Added hosted service

--- Niverobot/Niverobot.Consumer/Consumer.cs
+++ Niverobot/Niverobot.Consumer/Consumer.cs
@@ -1,17 +1,19 @@
 ﻿using System;
-using System.Timers;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-using Niverobot.Domain.EfModels;
+using Microsoft.Extensions.Hosting;
 using Niverobot.Interfaces;
 using Serilog;
 
 namespace Niverobot.Consumer
 {
-    public class Consumer
+    public class Consumer : IHostedService, IDisposable
     {
         private readonly IConfiguration _config;
         private readonly IReminderService _reminderService;
+        private Timer _timer;
 
         public Consumer(IConfiguration config, IReminderService reminderService)
         {
@@ -20,22 +22,42 @@ namespace Niverobot.Consumer
         }
 
         // Application starting point
-        public void Run()
+        // public void Run()
+        // {
+        //     try
+        //     {
+        //         // 10 Seconds interval
+        //         Timer timer = new Timer(10000);
+        //         timer.Elapsed += (sender, e) => RetrieveAndSendReminders();
+        //         timer.Start();
+        //
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         Log.Error("Error consuming reminder {0}", e);
+        //     }
+        // }
+        
+        public Task StartAsync(CancellationToken cancellationToken)
         {
-            try
-            {
-                // 10 Seconds interval
-                Timer timer = new Timer(10000);
-                timer.Elapsed += ( sender, e ) =>  RetrieveAndSendReminders();
-                timer.Start();
-                
-            }
-            catch (Exception e)
-            {
-                Log.Error("Error consuming reminder {0}", e);
-            }
+            _timer = new Timer(RetrieveAndSendReminders, null, TimeSpan.Zero, 
+                TimeSpan.FromSeconds(10));
+            
+            return Task.CompletedTask;
         }
 
+        public Task StopAsync(CancellationToken cancellationToken)
+        { 
+            Log.Information("Timed Hosted Service is stopping.");
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+        
         private void RetrieveAndSendReminders(object state = null)
         {
             Console.WriteLine("starting check {0}", DateTime.UtcNow);
@@ -45,7 +67,7 @@ namespace Niverobot.Consumer
                 var end = DateTime.UtcNow.AddMinutes(1);
                 var nextReminders = _reminderService.GetNotSendReminders(start).ToList();
                 var currentReminders = nextReminders.Where(x =>
-                   x.TriggerDate < end).ToList();
+                    x.TriggerDate < end).ToList();
                 foreach (var reminder in currentReminders)
                 {
                     _reminderService.SendReminderAsync(reminder);
