daee54ba30c6426e09731866b6e0c205801cd7cb
Rasjaad Basarat <rasjaad@Rasjaads-MacBook-Pro.local>
2020-08-18 15:22:25 +0200

    Clean up packages

--- Niverobot/Niverobot.Consumer/Consumer.cs
+++ Niverobot/Niverobot.Consumer/Consumer.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Niverobot.Interfaces;
 using Serilog;
